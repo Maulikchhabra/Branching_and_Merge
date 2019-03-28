7bc1c4731f30c0aa41ea5ac2b3a33f6ccf0b35b4
maulik <maulikchhabra@gmail.com>
2019-03-28 10:32:42 +0530

    master commit 1 (parent class declaration)

---
+++ f1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+ protected:
+ 
+    char name[50];
+    char gender;
+     int rollno;    
+};   // end of class student (PARENT CLASS)
