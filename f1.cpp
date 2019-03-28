4bdc404890cbcdb2971158300a22992e78c2f963
maulik <maulikchhabra@gmail.com>
2019-03-28 10:33:47 +0530

    master commit 2 (parent class function 1)

--- f1.cpp
+++ f1.cpp
@@ -7,5 +7,15 @@ class Student
  
     char name[50];
     char gender;
-     int rollno;    
+     int rollno;
+ public:
+     void student_Details()
+   {
+     cout<<"Enter your name\n";
+     cin>>name;
+     cout<<"\nEnter your gender\n";
+     cin>>gender;
+     cout<<"\nEnter your roll no.";
+     cin>>rollno;
+   }
 };   // end of class student (PARENT CLASS)
